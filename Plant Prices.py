73982ab8ed1b5cade62661d3fbbf29b7a00ec032
MasonGillis2001 <109878657+MasonGillis2001@users.noreply.github.com>
2022-07-27 10:53:00 -0500

    Add files via upload

---
+++ Plant Prices.py
@@ -0,0 +1,19 @@
+flowerpot = int(input('How many flowerpots? '))
+flower_seeds = int(input('How many packs of flower seeds? '))
+soil = int(input('How many bags of soil? '))
+
+flowerpot_price = 4.00
+flower_seeds_price = 1.00
+soil_price = 5.00
+tax_rate = 0.06
+
+flower_pot_total = flowerpot * flowerpot_price
+flower_seeds_total = flower_seeds * flower_seeds_price
+soil_total = soil * soil_price
+total = flower_pot_total + flower_seeds_total + soil_total
+tax = tax_rate * total
+grand_total = total + tax
+
+print(f'Your total for flower pots will be {flower_pot_total}, your total for seeds will be {flower_seeds_total}, your total for soil will {soil_total}, \
+your total amount of tax will be {tax}, and your grand total will be {grand_total}.')
+
